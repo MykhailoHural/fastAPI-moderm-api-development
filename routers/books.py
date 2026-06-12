d5cf712608c67ce0ef3c9d20e3e20ccda717c967
Mykhailo Hural <mykhailo.hural@crunch.is>
2026-06-12 15:50:06 +0300

    fix: updated .gitignore and fixed router bug

--- routers/books.py
+++ routers/books.py
@@ -9,7 +9,7 @@ async def get_books(count: int = 10, offset: int = 0):
     return books_db[offset : offset + count]
 
 
-@router.get("/{isbn}", response_model=list[Book])
+@router.get("/{isbn}", response_model=Book)
 async def get_books_by_id(isbn: str):
     for book in books_db:
         if book["isbn"] == isbn:
