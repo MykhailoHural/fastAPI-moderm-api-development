4f96611d2850d19f154bcaf9beae3412bd796c5d
Mykhailo Hural <mykhailo.hural@crunch.is>
2026-06-12 14:25:21 +0300

    feat: added list of the books to the API

---
+++ books_db.py
@@ -0,0 +1,5 @@
+books_db = [
+    {"name": "The Lord of the Rings", "isbn": "9876"},
+    {"name": "Harry Potter", "isbn": "3861"},
+    {"name": "Avengers", "isbn": "3376"},
+]
---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+from books_db import books_db
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Base(BaseModel): ...
+
+
+class Book(Base):
+    isbn: str
+    name: str
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello my book list"}
+
+
+@app.get("/books", response_model=list[Book])
+async def get_books():
+    return books_db
+
+
+@app.get("/books/{isbn}")
+async def get_books_by_id(isbn: str):
+    for book in books_db:
+        if book["isbn"] == isbn:
+            return book
+
+    raise HTTPException(status_code=404, detail="Книгу не знайдено")
