b923d472d523ac205eed7f18e03fab37037a297c
Mykhailo Hural <mykhailo.hural@crunch.is>
2026-06-12 15:18:59 +0300

    feat: organise structure of the project

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from books_db import books_db
-from pydantic import BaseModel
+from fastapi import FastAPI
+from routers import books
 
 app = FastAPI()
-
-
-class Base(BaseModel): ...
-
-
-class Book(Base):
-    isbn: str
-    name: str
 
 
 @app.get("/")
@@ -18,22 +9,4 @@ async def root():
     return {"message": "Hello my book list"}
 
 
-@app.get("/books", response_model=list[Book])
-async def get_books(count: int = 10, offset: int = 0):
-    return books_db[offset : offset + count]
-
-
-@app.get("/books/{isbn}")
-async def get_books_by_id(isbn: str):
-    for book in books_db:
-        if book["isbn"] == isbn:
-            return book
-
-    raise HTTPException(status_code=404, detail="Книгу не знайдено")
-
-
-@app.post("/books")
-async def post_book(book:Book) -> Book:
-    book_record = book.model_dump()
-    books_db.append(book_record)
-    return book
+app.include_router(books.router, prefix="/books")
---
+++ routers/books.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, HTTPException
+from books_db import books_db
+from schemas.books import Book
+router = APIRouter()
+
+
+@router.get("/", response_model=list[Book])
+async def get_books(count: int = 10, offset: int = 0):
+    return books_db[offset : offset + count]
+
+
+@router.get("/{isbn}", response_model=list[Book])
+async def get_books_by_id(isbn: str):
+    for book in books_db:
+        if book["isbn"] == isbn:
+            return book
+
+    raise HTTPException(status_code=404, detail="Книгу не знайдено")
+
+
+@router.post("/")
+async def post_book(book: Book) -> Book:
+    book_record = book.model_dump()
+    books_db.append(book_record)
+    return book
